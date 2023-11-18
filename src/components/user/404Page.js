09b6c2dcecaed5ca73bac8faf5ae024e8cb0e045
Juho-Korhonen <juho.korhonent@gmail.com>
2023-11-18 14:12:11 +0200

    404 title added

--- src/components/user/404Page.js
+++ src/components/user/404Page.js
@@ -1,5 +1,6 @@
 import NavBar from "../inclusive/NavBar"
 import { useEffect, useState } from "react";
+import { Helmet } from 'react-helmet-async'
 
 export function NoPage(){
     const [mobile, setMobile] = useState(window.innerWidth <= 500);
@@ -17,6 +18,10 @@ export function NoPage(){
 
     return !mobile ? (
         <main>
+            <Helmet>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                <title>Uriel Tekoäly Chat - 404</title>
+            </Helmet>
             <NavBar/>
             <h1 style={{marginInline: 20}}>Tässä osoitteessa ei ole mitään, haluatko siirtyä etusivulle?</h1>
             <button style={{backgroundColor: '#3994FF', color:'white', marginInline: 30}} onClick={() => window.location.replace("https://urielai.com/etusivu")}>Siirry takaisin etusivulle</button>
@@ -24,6 +29,10 @@ export function NoPage(){
     ):
     (
         <main  style={{position: 'absolute', top: 0, alignItems: 'center', textAlign: 'center'}}>
+            <Helmet>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                <title>Uriel Tekoäly Chat - 404</title>
+            </Helmet>
             <NavBar/>
             <h1>Tässä osoitteessa ei ole mitään, haluatko siirtyä etusivulle?</h1>
             <button style={{backgroundColor: '#3994FF', color:'white'}} onClick={() => window.location.replace("https://urielai.com/etusivu")}>Siirry takaisin etusivulle</button>
